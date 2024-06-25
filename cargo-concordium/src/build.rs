0497bf603c9ac8f403e50b1d1829ad32b1868415
Lasse Møldrup <lasse.moeldrup@gmail.com>
2024-06-25 15:38:11 +0200

    Change default build output to concordium-out/module.wasm.v1

--- cargo-concordium/src/build.rs
+++ cargo-concordium/src/build.rs
@@ -397,20 +397,49 @@ pub(crate) fn build_contract(
         }
     }
 
+    let (metadata, args_without_manifest) = get_crate_metadata(cargo_args)?;
+
+    let package = metadata
+        .root_package()
+        .context("Unable to determine package.")?;
+
+    let package_root = package
+        .manifest_path
+        .parent()
+        .context("Unable to get package root path.")?;
+
+    let wasm_file_name = format!("{}.wasm", to_snake_case(package.name.as_str()));
+
+    let package_root_path = package_root.canonicalize()?;
+
+    let package_version_string = format!("{}-{}", package.name, package.version);
+
     // If the output path is supplied make sure up-front before building anything
     // that it points to a sensible location
-    if let Some(out) = &out {
-        // A path and a filename need to be provided when using the `--out` flag.
-        if out.file_name().is_none() || out.is_dir() {
-            anyhow::bail!(
-                "The `--out` flag requires a path and a filename (expected input: \
-                 `./my/path/my_smart_contract.wasm.v1`)"
-            );
+    let out_filename = match &out {
+        Some(out) => {
+            // A path and a filename need to be provided when using the `--out` flag.
+            if out.file_name().is_none() || out.is_dir() {
+                anyhow::bail!(
+                    "The `--out` flag requires a path and a filename (expected input: \
+                     `./my/path/my_smart_contract.wasm.v1`)"
+                );
+            }
+            out.clone()
         }
-        if let Some(out_dir) = out.parent() {
-            fs::create_dir_all(out_dir)
-                .context("Unable to create directory for the resulting smart contract module.")?;
+        None => {
+            let extension = match version {
+                WasmVersion::V0 => "v0",
+                WasmVersion::V1 => "v1",
+            };
+            let relative_path = format!("concordium-out/module.wasm.{}", extension);
+            package_root_path.join(relative_path)
         }
+    };
+
+    if let Some(out_dir) = out_filename.parent() {
+        fs::create_dir_all(out_dir)
+            .context("Unable to create directory for the resulting smart contract module.")?;
     }
 
     #[allow(unused_assignments)]
@@ -458,23 +487,6 @@ pub(crate) fn build_contract(
             }
         }
     };
-
-    let (metadata, args_without_manifest) = get_crate_metadata(cargo_args)?;
-
-    let package = metadata
-        .root_package()
-        .context("Unable to determine package.")?;
-
-    let package_root = package
-        .manifest_path
-        .parent()
-        .context("Unable to get package root path.")?;
-
-    let wasm_file_name = format!("{}.wasm", to_snake_case(package.name.as_str()));
-
-    let package_root_path = package_root.canonicalize()?;
-
-    let package_version_string = format!("{}-{}", package.name, package.version);
 
     let (out_filename, wasm, stored_build_info) = if let Some(image) = image {
         let cwd = env::current_dir()
@@ -553,17 +565,6 @@ pub(crate) fn build_contract(
                 output_wasm_file.display()
             )
         })?;
-
-        let out_filename = match out {
-            Some(out) => out,
-            None => {
-                let extension = match version {
-                    WasmVersion::V0 => "v0",
-                    WasmVersion::V1 => "v1",
-                };
-                PathBuf::from(format!("{}.{}", output_wasm_file.display(), extension))
-            }
-        };
 
         (out_filename, wasm, None)
     };
